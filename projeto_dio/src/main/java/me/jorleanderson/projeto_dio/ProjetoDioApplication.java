642e3997394c13f14a5834f9d7226a09a9bc3bb5
Jorleanderson <jorleandersonmoraes@gmail.com>
2025-03-03 12:02:12 -0300

    primeiro comit

---
+++ projeto_dio/src/main/java/me/jorleanderson/projeto_dio/ProjetoDioApplication.java
@@ -0,0 +1,13 @@
+package me.jorleanderson.projeto_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioApplication.class, args);
+	}
+
+}
